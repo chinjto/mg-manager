4396365e3464b08a133c6466747077a24d5da692
chinjto <contact@cyril-defaye.fr>
2025-09-23 01:24:56 +0200

    test(cypress): Ajout captures app + header + menu
    - Ajout d'une capture de la page d'accueil
    - Ajout d'une capture du header
    - Ajout d'une capture du menu

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,12 @@
 describe('Starting test', () => {
   it('Visits the web app', () => {
     cy.visit('/')
-    cy.get('app-root')
+    cy.get('.layout-topbar').invoke('css', 'position', 'absolute')
+    cy.get('.layout-sidebar').invoke('css', 'position', 'absolute')
+    cy.get('.layout-wrapper').screenshot('app')
+    cy.get('.layout-topbar').invoke('css', 'position', null)
+    cy.get('.layout-sidebar').invoke('css', 'position', null)
+    cy.get('.layout-topbar').screenshot('layout/topbar')
+    cy.get('.layout-sidebar').screenshot('layout/sidebar')
   })
 })
